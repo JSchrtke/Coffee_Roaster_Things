0fe252424814eda9a6c5fdf2e55a1b1f3ac73bee
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-10-16 19:06:05 +0200

    added data saving to file; added yes/no menu method

--- temp_sensor.py
+++ temp_sensor.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import threading
+import numpy as np
 import matplotlib.pyplot as plt
 
 from Phidget22.Phidget import *
@@ -7,134 +9,201 @@ from Phidget22.Devices.TemperatureSensor import *
 from Phidget22.PhidgetException import *
 
 
-def storeTemperatureData(time, temperature, storage_list_x, storage_list_y):
+class InputError(Exception):
     """
-    * Stores the data, time and temperature pairs, in a given list
-    * time: the time associated with a temperature measurement
-    * temperature: the temperature measured
-    * storage_list: a particular list used for storing the data Tuples
+    Exception raised for input errors
     """
-    storage_list_x.append(time)
-    storage_list_y.append(temperature)
+    def __init__(self, error_message):
+        self.error_message = error_message
 
 
-def displayError(e):
+class Threading(threading.Thread):
+
+    def __init__(self, thread_id, thread_name, method):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.thread_name = thread_name
+        self.method = method
+
+    def run(self):
+        print("starting " + self.thread_name)
+        print(str(self.thread_name) + " called: " + str(self.method))
+        self.method()
+        print("terminating " + self.thread_name)
+
+
+def store_temp_data(time, temp, time_storage, temp_storage):
+    """
+    * Stores temperature and time data in two corresponding lists
+    * args:
+        - time: the time associated with a temperature measurement
+        - temp: the temperature measured
+    """
+    time_storage.append(time)
+    temp_storage.append(temp)
+
+
+def display_error(self):
     """
     * Displays the error details
     * Tells the user what to do in case of an error/exception
     * e: The exception to be handled
     """
+    e = self
+
     sys.stderr.write("Code: " + str(e.code) + "\n")
     sys.stderr.write("Desc: " + e.details + "\n")
 
-    if (e.code == ErrorCode.EPHIDGET_TIMEOUT):
-        sys.stderr.write("\nThis error most likely occurs because the Phidget isn't properly attached to the computer. Please check the connection and try again. ")
+    if (self.e.code == ErrorCode.EPHIDGET_TIMEOUT):
+        sys.stderr.write("\nThis error most likely occurs because the Phidget isn't properly"
+                         "attached to the computer. Please check the connection and try again. ")
         input("\nPress enter to continue.\n")
 
-def onAttachHandler(self):
-    """
-    * Set's the DataInterval and ChangeTrigger
-    * Get's called when a channel registers an attachment event
-    """
-    # creating an instance of the TemperatureSensor class to assign the 
-    # triggers to
-    tempPhidget = self
 
+def on_attach_handler(self):
+    """
+    * Get's called when a channel registers an attachement event
+    * sets:
+        - Device serial
+        - Hub Port
+        - Channel
+        - Temperature change trigger value
+        - Data polling interval
+    """
+    temp_sensor = self
     try:
-        tempPhidget.setTemperatureChangeTrigger(0.0)
-        tempPhidget.setDataInterval(50)
+        temp_sensor.setDeviceSerialNumber(118560)
+        temp_sensor.setHubPort(0)
+        temp_sensor.setChannel(0)
+        # Temperature change threshold in °C
+        temp_sensor.setTemperatureChangeTrigger(0)
+        # Data polling interval in ms
+        temp_sensor.setDataInterval(50)
     except PhidgetException as e:
         print("\nError while setting the attach handler!")
-        displayError(e)
+        self.display_error(e)
 
 
-def onTemperatureChangeHandler(self, temperature):
+def on_temperature_change_handler(self, temp):
     """
-    * Gets called when the temperature change is over the set threshold
+    * Get's called whenever a temperature change even occurs, i.e. whenever the temperature
+        change goes over the value defined with setTemperatureChangeTrigger()
+    * temp: the measured temperature value
     """
     time_now = time.time() - time_start
-    
-    # prints a percentage value to let the user know how much data has been 
-    # collected during collection
-    print("\r" + str(round((time_now / sample_time * 100), 3)) + "%" + "...", end = " ")
+    # prints a percentage value to let the user know how much data has been collected during
+    # collection
+    print("\r" + str(round((time_now / sample_time * 100), 3)) + "%"
+          + "...\tTime: " + str(round(time_now, 2)) + "s\tTemperature: "
+          + str(round(temp, 2)) + "°C", end=" ")
+    store_temp_data(time_now, temp, test_storage_x, test_storage_y)
 
-    storeTemperatureData(time_now, temperature, test_storage_x, test_storage_y)
 
+def yes_no_menu(default):
+    string = sys.stdin.readline(5)
+    if not string:
+        raise InputError("Empty input!")
+
+    if (string[0] == '\n'):
+        if (default == -1):
+            raise InputError("Empty input!")
+        return default
+
+    if (string[0] == 'n' or string[0] == 'N'):
+        return False
+
+    if (string[0] == 'y' or string[0] == 'Y'):
+        return True
+
+    raise InputError("Invalid input!")
+
+
+def save_profile_to_file(file_name, time_storage, temperature_storage):
+    try:
+        with open("C:\\Users\\Joharnis\\Desktop\\Coffee Roaster Testing\\data\\" + str(file_name)
+                  + ".txt", "w") as filehandle:
+
+                for time_value in time_storage:
+                    filehandle.write("%s\n" % time_value)
+
+                filehandle.write("\n")
+
+                for temp_value in temperature_storage:
+                    filehandle.write("%s\n" % temp_value)
+
+                filehandle.write("\n")
+                filehandle.close()
+    except Exception as e:
+        display_error(e)
+
+
+# Test list for storing the time and temp data, need to be removed and replaced when implementing
+# future storage/plotting system
+test_storage_x = []
+test_storage_y = []
 
 # Creating an instance of the TemperatureSensor object
-tempSens = TemperatureSensor()
-
-# Define serials, channels and such to connect phidget
-tempSens.setDeviceSerialNumber(118560)
-tempSens.setHubPort(0)
-tempSens.setChannel(0)
+temp_sens = TemperatureSensor()
 
 # Setting a starting time for timestamp use later
 time_start = time.time()
 
-# Test list for storing the time and temp data, need to be removed and replaced
-# when implementing future storage/plotting system
-test_storage_x = []
-test_storage_y = []
 
-# Setting the AttachHandler and TempChangeHandler to the previously defined 
-# functions
+# Setting the time data will be sampled for
+sample_time = 900
+
+# Setting the AttachHandler and TempChangeHandler to the previously defined functions
 """
-Setting the onTemperatureChangeHandler is necessary, even if the defined 
-handler doesn't execute any code in it's body.
-For the program be able to return a temperature via any method (specifically
-the getTemperature() method), it is necessary to set the 
-onTemperatureChangeHandler, wait for atleast one DataInterval and then un-set
-the onTemperatureChangeHandler in order for the program to have atleast one 
-temperature value to return. Otherwise, methods that try to return the 
-temperature will throw an exception
+Setting the onTemperatureChangeHandler is necessary, even if the defined handler doesn't
+any code in it's body. For the program be able to return a temperature via any method
+(specifically the getTemperature() method), it is necessary to set the
+onTemperatureChangeHandler, wait for atleast one DataInterval and then un-set the
+onTemperatureChangeHandler in order for the program to have atleast one temperature value to
+return. Otherwise, methods that try to return the temperature will throw an exception
 """
-tempSens.setOnTemperatureChangeHandler(onTemperatureChangeHandler)
-tempSens.setOnAttachHandler(onAttachHandler)
+temp_sens.setOnAttachHandler(on_attach_handler)
+temp_sens.setOnTemperatureChangeHandler(on_temperature_change_handler)
 
 
 # Waiting for the phidget to be attached
 while True:
     print("Opening an waiting for Attachement...")
     try:
-        tempSens.openWaitForAttachment(5000)
+        temp_sens.openWaitForAttachment(5000)
         break
     except PhidgetException as e:
         print("\nError in attachement event!")
-        displayError(e)
+        display_error(e)
 print("Attached!")
-
-# Setting the time data will be sampled for
-sample_time = 10
 print("Sampling data for " + str(sample_time) + " seconds...")
 
-# This sleep timer suspeds the further execution of the program until the data 
-# collection has finished
+# This sleep timer suspeds the further execution of the program
+# until the data collection has finished
 time.sleep(sample_time)
 
-# Newline here is needed because of the way the progress counter works, see 
-# in def of onTemperatureChangeHandler
-print("\r100"+ "%" + "...")
+print("\r100" + "%" + "...")
 print("Done sampling...")
 
 # Clearing the TemperatureChangeHandler
 print("Cleaning up...")
-tempSens.setOnTemperatureChangeHandler(None)
+temp_sens.setOnTemperatureChangeHandler(None)
 print("Done cleaning up.")
 
-#TESTING-----------------------------------------------------------------------
-# Printing the storage list as a test
-print(test_storage_x)
-print(test_storage_y)
-plt.plot(test_storage_x, test_storage_y)
-plt.xlabel("time in s")
-plt.ylabel("temperature in C")
-ax = plt.gca()
-#ax.set_ylim([0, 500])
-ax.set_xlim([0, sample_time])
-plt.show()
-#END---------------------------------------------------------------------------
+while True:
+    print("\nSave current data? [y/n]")
+    try:
+        yes_no = yes_no_menu(-1)
+        if yes_no is True:
+            save_profile_to_file(input("Enter filename: \n"), test_storage_x, test_storage_y)
+            print("saving...")
+            time.sleep(1)
+            break
+        elif yes_no is False:
+            break
+        else:
+            pass
+    except InputError as e:
+        pass
 
-# Wait's for the user to press Enter, or any key really, to terminate the 
-# program
+# Wait's for the user to press Enter, or any key really, to terminate the program
 input("Press Enter to exit:\n")
