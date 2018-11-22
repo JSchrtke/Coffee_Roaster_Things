d1cb1d7ec32adbe870c0b71b3005e207d3bb82bf
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-11-22 18:48:33 +0100

    Added RoastProfile class (WIP) RoastProfile: added __init__ ()(WIP) RoastProfile: added load_reference()(WIP) RoastProfile: added start_roast()(WIP)

---
+++ RoastProfile.py
@@ -0,0 +1,21 @@
+class RoastProfile():
+    def __init__(self):
+        # TODO: figure out what needs to go in here
+        print("DEBUG: RoastProfile.__init__() was called")
+
+    # create load_reference in RoastProfile class
+    def load_reference(self, path):
+        """
+        Loads an existing roast profile to be used as a reference for another roast
+        * path: The path to the roast profile to be used as reference
+        """
+        # TODO: figure out how this is supposed to work
+        print("DEBUG: RoastProfile.load_reference() was called; path: " + str(path))
+
+    # create start_roast method in RoastProfile
+    def start_roast(self, ref_prof):
+        """
+        starts the roasting process, either with or without a reference profile
+        """
+        # TODO: finish this method
+        print("DEBUG: RoastProfile.start_roast() was called")
