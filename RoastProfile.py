17469720288ff3fa1a2882ac60f10f60b4cf322e
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-11-28 18:08:34 +0100

    export_profile(): added handling for invalid file names

--- RoastProfile.py
+++ RoastProfile.py
@@ -1,6 +1,7 @@
 import time
 import sys
 import csv
+import os
 
 
 class RoastProfile():
@@ -20,6 +21,7 @@ class RoastProfile():
         self.reference_data = []
         # create a variable for the current data
         self.live_data = []
+        # TODO: need to create a list with temperatures and their associated voltages to have it for comparison
         # create variable to hold the temperature sensor
         self.sensor = temp_probe
 
@@ -36,7 +38,8 @@ class RoastProfile():
             self.reference_data = list(csv_reader)
             # close the reference profile file
             file.close()
-        print("DEBUG: RoastProfile.import_profile() was called; path: " + str(path))  # TODO: remove this once the method is done
+            # TODO: splitting the reference data up into time and temp in seperate lists
+                # TODO: after splitting, put in a check to see if they still have the same number of elements, just to be sure
         # TODO: DEBUG CODE, REMOVE ONCE DONE!
         for data in self.reference_data:
             print("DEBUG: Index: " + str(self.reference_data.index(data)) + " Data: " + str(data))
@@ -46,7 +49,6 @@ class RoastProfile():
         Saves the current roast to file
         * data_list: A list with the data to be saved
         """
-        # TODO: make some mechanism to have default values for the path and name
         # create variable to hold the path for the profile folder
         path = None
         # create variable for the profile name
@@ -54,14 +56,26 @@ class RoastProfile():
         # ask user for path
         path = input("Enter the folder path to where the profile should be saved:\n")
         # ask user for a name for the profile
-        name = input("Enter a name for the profile to be saved:\n")
-        print("Saving profile...")
+        name = input("Enter a name for the profile to be saved; must include .csv ending!:\n")
+        # creating the file path name variable
+        file_path_name = os.path.join(path, name)
         # open path/ create file
-        with open(path + name + ".csv", "w", newline="") as file:  # empty newline is there to prevent the \r
-            csv_app = csv.writer(file, quoting=csv.QUOTE_ALL)
-            csv_app.writerows(data_list)
-        file.close()
-        print("Saved!")
+        # while loop to repeat after exception has been raised
+        while True:
+            try:
+                print("Saving profile...")
+                with open(file_path_name, "w", newline="") as file:  # empty newline is there to prevent the \r
+                    csv_app = csv.writer(file, quoting=csv.QUOTE_ALL)
+                    csv_app.writerows(data_list)
+                file.close()
+                print("Saved!")
+                break
+            except FileNotFoundError:
+                print("Error! Invalid file path or name! Saving to Desktop as default.csv")
+                path = os.path.abspath(os.path.expanduser("~/Desktop"))
+                name = "default.csv"
+                file_path_name = os.path.join(path, name)
+                continue
 
     def set_target_temperature(self, temp_value):
         """
@@ -79,7 +93,7 @@ class RoastProfile():
         time_current = time.time() - time_start
         return time_current
 
-    # TODO: create method to get current temperature
+    # create method to get current temperature
     def get_current_temp(self):
         """
         Get's a temperature measured by self.sensor and returns it
@@ -106,7 +120,7 @@ class RoastProfile():
 
 
             # TODO: DEBUG CODE, REMOVE ONCE DONE
-            debug_while_loop_sentry = 10
+            debug_while_loop_sentry = 10  # set this to the time in seconds you want the loop to run for
             debug_while_loop_counter = 0
 
 
@@ -121,16 +135,15 @@ class RoastProfile():
                 # get the current temperature, store in it's variable
                 self.temperature_current = self.get_current_temp()
                 print("DEBUG: self.temperature_current: " + str(self.temperature_current))
-                # TODO: find the closest time value in the reference profile to the current time value in the live roast
-                    # TODO: create method to find the closest value to a given value in a list
-                # TODO: compare it's temperature value to the current temperature in the live roast
-                    # TODO: create a method to take to values and calculate the difference between them
-                # TODO: calculate correction factor based on the temperature differential
-                    # TODO: is the temp differenc between live and ref is larger than x, try to correct
-                            # how big is x? make is so this can be set dynamically
-                            # this is probably some lookup table, so use the method to find a close value to a given value for it
-                # TODO: send correction factor to arduino
-                    # TODO: create arduino interface
+                # TODO: comparison method:
+                    # what do I need to do?
+                        # compare the current temperature value with the reference temperature value corresponding to the same time
+                        # so I need to first get the current time, then find the closest value in the list with the reference times and get it's index
+                            # this needs to be a general purpose method, probably goes into the helper functions
+                        # then get the temperature value with the same index from it's list
+                            # put that into the temperature_target variable
+                        # calculate the diff between the target value and the current value
+                        # if the difference is larger than 0.5°C, lookup the voltage value associated with the target temperature and send it to the arduino
                 # create time and temp tuple
                 data_tuple = (self.time_current, self.temperature_current)
                 # append time and temperature tuple to a list that will be used to later store the profile of the roast
@@ -138,14 +151,14 @@ class RoastProfile():
                 # TODO: check if the current time is greater or equal than the biggest time value in the reference profile
                     # TODO: if yes, set a variable that is used to check if the roast is finished to true
                     # TODO: if no, repeat unitl it is
-                # create variable that get's the time at loop end
-                time_at_loop_end = time.time()
                 # create mechanism to handle if the time at end is bigger than the time at
                 # start + 1
                 debug_while_loop_counter = debug_while_loop_counter + 1  # TODO: DEBUG LINE, REMOVE
                 try:
-                    # make loop wait by the difference to the start time + 1 second and the end time
-                    time.sleep(1 - ((time.time() - time_at_loop_start) % 1))
+                    # create variable that get's the time at loop end
+                    time_at_loop_end = time.time()
+                    # make loop wait by 1 second minus the time it took to execute the code
+                    time.sleep(1 - (time_at_loop_end - time_at_loop_start))
                 except ValueError:
                     print("DEBUG: ValueError was raised at the end of"
                           + " RoastProfile.start_roast while loop")
@@ -154,7 +167,6 @@ class RoastProfile():
 
             # if no:
         else:
-            print("DEBUG: There is no reference profile")  # TODO: remove once rest is functional
             # tell user roaster is running in manual mode
             print("No reference profile was loaded, running roaster in manual mode!")
             # prompt user to enter a starting temperature for the roaster to be; store it
