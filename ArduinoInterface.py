777b3ac4f13e16cfb9132160951168c1c4b44b44
Johannes Schrötke <johannes.schroetke@gmail.com>
2018-11-28 14:55:47 +0100

    added ArduinoInterface class

---
+++ ArduinoInterface.py
@@ -0,0 +1,15 @@
+class ArduinoInterface():
+    """
+    class that will handle all things arduino
+    """
+    def __init__(self):
+        # TODO: There needs to be something here at some point
+        print("DEBUG: called ArduinoInterface.__init__()")
+
+    def send_data(self):
+        """
+        This method handles all the data transfer from the roaster program running on a pc to the
+        arduino that controls the heating element and so on
+        """
+        # TODO: figure out how this is gonna work
+        print("DEBUG: called ArduinoInterface.send_data()")
